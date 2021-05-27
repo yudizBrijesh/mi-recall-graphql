a3d5b568e235add862253514792597e0fd32d512
yudiz_brijesh <brijesh.a@yudiz.in>
2021-05-27 11:05:46 +0530

    INITIAL COMMIT

--- app.js
+++ app.js
@@ -30,8 +30,8 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app });
 
-app.listen(4000, () => {
-    console.log(`🚀  Server ready at http://localhost:4000`);
+app.listen(4000 || process.env.PORT, () => {
+    console.log(`🚀  Server ready at http://localhost:${process.env.PORT}`);
 });
 
 module.exports = app;
